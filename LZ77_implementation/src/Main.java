0d93a05f9a9c61fab3b8d855629e7e94a531e88a
Mohamed Fathi <Mohamedfathi11119@gmail.com>
2023-10-29 23:02:03 +0200

    decompression function added and Work Well
    need Unit testing Only

--- LZ77_implementation/src/Main.java
+++ LZ77_implementation/src/Main.java
@@ -1,12 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
+import java.io.FileWriter;
 
 public class Main {
 
     public static int longestMatch(String data, int i , int j){
         int res = -1 ;
-        // i=3  j = 5 ABCABC
         for (int k = 0; k <= i-1; k++) {
              int win = k ;
              int my = i;
@@ -58,12 +59,51 @@ public class Main {
             e.printStackTrace();
         }
     }
-    public void deComp(){
+    public static void deComp(File file){
+        //System.out.println(arr[0].substring(1) +"---" +arr[1]+"---" +arr[2].substring(0,arr[2].length()-1));
+        String data ="";
+        try{
+            Scanner read  = new Scanner(file);
+            while (read.hasNextLine()){
+                String[] arr = read.nextLine().split(",");
+                int start = Integer.parseInt(arr[0].substring(1));
+                int count = Integer.parseInt(arr[1]);
+
+                if (start == 0 && count == 0){
+                    data += arr[2].substring(0,arr[2].length()-1);
+                }
+                else {
+                    int len =data.length();
+                    for (int i = 0; i < count ; i++) {
+                        //System.out.println( "next = "+arr[2].substring(0,arr[2].length()-1)+"  start="+start+ "  count = "+ count + "  datalen = "+data.length() + "  =>>>"+data);
+                        data += data.charAt(len-start);
+                        start--;
+                    }
+                    data += arr[2].substring(0,arr[2].length()-1);
+                }
+
+            }
+
+        }
+        catch (FileNotFoundException e ){
+            e.printStackTrace();
+        }
+        try{
+            FileWriter f = new FileWriter("result.txt",false);
+            f.write(data);
+            f.close();
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+
 
     }
     public  static void main(String[] args) {
         File f = new File("test.txt");
-        comp(f);
+        deComp(f);
         //  comp or deComp ?
         // take file name
         // validate file name
